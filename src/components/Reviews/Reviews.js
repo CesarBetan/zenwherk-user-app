99835c36ade894e79940a8b37bc2e2a9be74180d
Eduardo Valencia <l.alo.red@hotmail.com>
2018-04-15 21:02:00 -0500

    Add Reviews Component

---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Reviews.css';
+
+class Reviews extends Component {
+
+    render() {
+        return (
+            <p>Reviewx component!</p>
+        );
+    }
+}
+
+export default Reviews;
