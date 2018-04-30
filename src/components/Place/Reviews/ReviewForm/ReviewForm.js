8dc7719ea6716e67e845da75a055ea13b04cd31f
Cesar Betancourt <cesarebetan@gmail.com>
2018-04-29 23:10:59 -0500

    User can post a review of a place

--- src/components/Place/Reviews/ReviewForm/ReviewForm.js
+++ src/components/Place/Reviews/ReviewForm/ReviewForm.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import './ReviewForm.css';
-import SectionTitle from "../../../SectionTitle/SectionTitle";
 import NavBar from "../../../NavBar/NavBar";
 import {apiUrl} from "../../../../Constants";
 import axios from "axios/index";
+import filledStar from '../../../../assets/Place/Filled Star.svg';
+import emptyStar from '../../../../assets/Place/Empty Star.svg';
 
 class ReviewForm extends Component {
 
     constructor(props) {
         super(props);
         let uuid = this.props.match.params.uuid;
-        this.state = { place : [], uuid: uuid };
+        let user = JSON.parse(localStorage.getItem('user'));
+        this.state = { place : [], endpoint: apiUrl+"review/", uuid: uuid, uuidUser: user.uuid, rating: 1 , reviewDescription: ""};
         this.getPlace = this.getPlace.bind(this);
+
+        this.handleReviewDescription = this.handleReviewDescription.bind(this);
+        this.changeRating = this.changeRating.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentWillMount() {
         this.getPlace()
+    }
+
+    handleReviewDescription(event) {
+        this.setState({reviewDescription: event.target.value});
+    }
+    closeAccountSuccess(event) {
+        document.getElementById("review-success").style.display = "none";
+    }
+    closeAccountError(event) {
+        document.getElementById("review-error").style.display = "none";
+    }
+    closeAccountPreError(event) {
+        document.getElementById("review-pre-error").style.display = "none";
     }
 
     getPlace() {
@@ -34,6 +53,68 @@ class ReviewForm extends Component {
         })
     }
 
+    changeRating(event){
+        switch(event.target.value){
+            case '5':
+                this.setState({rating: 5});
+                break;
+            case '4':
+                this.setState({rating: 4});
+                break;
+            case '3':
+                this.setState({rating: 3});
+                break;
+            case '2':
+                this.setState({rating: 2});
+                break;
+            case '1':
+                this.setState({rating: 1});
+                break;
+            default:
+                this.setState({rating: 1});
+                break;
+        }
+
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.setState({errorMessage: ""})
+        if(this.state.reviewDescription.length < 5){
+            this.setState({errorMessage: "La reseña debe tener al menos 3 caracteres."});
+            document.getElementById("review-success").style.display = "none";
+            document.getElementById("review-error").style.display = "none";
+            document.getElementById("review-pre-error").style.display = "block";
+        }else{
+            if(localStorage.getItem("accesstoken")){
+                const config = {
+                    headers:{'Authorization':'Bearer ' + localStorage.getItem("accesstoken")}
+                };
+                console.log(this.state.uuid);
+                axios.post(this.state.endpoint, {
+                    reviewRating: this.state.rating,
+                    reviewText: this.state.reviewDescription,
+                    place: {
+                        uuid: this.state.uuid
+                    },
+                    user: {
+                        uuid: this.state.uuidUser
+                    }
+                  }, config
+                ).then(res => {
+                    document.getElementById("review-error").style.display = "none";
+                    document.getElementById("review-pre-error").style.display = "none";
+                    document.getElementById("review-success").style.display = "block";
+                }).catch(err => {
+                    this.setState({errorServer: err.response.data.message});
+                    document.getElementById("review-pre-error").style.display = "none";
+                    document.getElementById("review-success").style.display = "none";
+                    document.getElementById("review-error").style.display = "block";
+                });
+            }
+        }
+    }
+
     render() {
         return (
           <div className="review-form-view">
@@ -41,41 +122,78 @@ class ReviewForm extends Component {
               <div>
                   <form className="form-style-user" onSubmit={this.handleSubmit}>
                       <ul className="PraxisNext-Bold">
-                          <div className="isa_success" id="account-success">
-                              La cuenta fue actualizada correctamente.
+                          <div className="isa_success" id="review-success">
+                              Se ha creado tu review exitosamente.
                               <a onClick={this.closeAccountSuccess}>X</a>
                           </div>
-                          <div className="isa_error" id="account-pre-error">
+                          <div className="isa_error" id="review-pre-error">
                               {this.state.errorMessage}
                               <a onClick={this.closeAccountPreError}>X</a>
                           </div>
-                          <div className="isa_error" id="account-error">
+                          <div className="isa_error" id="review-error">
                               {this.state.errorServer}
                               <a onClick={this.closeAccountError}>X</a>
                           </div>
                           <span className="review-form-title PraxisNext-Ultra">Review</span>
                           <span className="review-form-place PraxisNext-ExtraBlack">{this.state.place.name}</span>
-                          <span className="starRating">
-                              <input id="rating5" type="radio" name="rating" value="5"/>
-                              <label htmlFor="rating5">5</label>
-                              <input id="rating4" type="radio" name="rating" value="4"/>
-                              <label htmlFor="rating4">4</label>
-                              <input id="rating3" type="radio" name="rating" value="3"/>
-                              <label htmlFor="rating3">3</label>
-                              <input id="rating2" type="radio" name="rating" value="2"/>
-                              <label htmlFor="rating2">2</label>
-                              <input id="rating1" type="radio" name="rating" value="1"/>
-                              <label htmlFor="rating1">1</label>
-                            </span>
+                          <li className="li-star-rating">
+                            <div className="star-rating">
+                                <input id="rating1" type="radio" name="rating" defaultChecked value="1" onClick={this.changeRating}/>
+                                <label htmlFor="rating1">
+                                    <img className="search-result-cell-star-image"
+                                         alt={(this.state.rating > 0 && this.state.rating !== undefined)
+                                           ? "Filled Star" : "No Fill Star"}
+                                         src={(this.state.rating > 0 && this.state.rating !== undefined)
+                                           ? filledStar : emptyStar}
+                                    />
+                                </label>
+                                <input id="rating2" type="radio" name="rating" value="2" onClick={this.changeRating}/>
+                                <label htmlFor="rating2">
+                                    <img className="search-result-cell-star-image"
+                                         alt={(this.state.rating > 1 && this.state.rating !== undefined)
+                                           ? "Filled Star" : "No Fill Star"}
+                                         src={(this.state.rating > 1 && this.state.rating !== undefined)
+                                           ? filledStar : emptyStar}
+                                    />
+                                </label>
+                                <input id="rating3" type="radio" name="rating" value="3" onClick={this.changeRating}/>
+                                <label htmlFor="rating3">
+                                    <img className="search-result-cell-star-image"
+                                         alt={(this.state.rating > 2 && this.state.rating !== undefined)
+                                           ? "Filled Star" : "No Fill Star"}
+                                         src={(this.state.rating > 2 && this.state.rating !== undefined)
+                                           ? filledStar : emptyStar}
+                                    />
+                                </label>
+                                <input id="rating4" type="radio" name="rating" value="4" onClick={this.changeRating}/>
+                                <label htmlFor="rating4">
+                                    <img className="search-result-cell-star-image"
+                                         alt={(this.state.rating > 3 && this.state.rating !== undefined)
+                                           ? "Filled Star" : "No Fill Star"}
+                                         src={(this.state.rating > 3 && this.state.rating !== undefined)
+                                           ? filledStar : emptyStar}
+                                    />
+                                </label>
+                                <input id="rating5" type="radio" name="rating" value="5" onClick={this.changeRating}/>
+                                <label htmlFor="rating5">
+                                    <img className="search-result-cell-star-image"
+                                         alt={(this.state.rating > 4 && this.state.rating !== undefined)
+                                           ? "Filled Star" : "No Fill Star"}
+                                         src={(this.state.rating > 4 && this.state.rating !== undefined)
+                                           ? filledStar : emptyStar}
+                                    />
+                                </label>
+                            </div>
+                          </li>
                           <li>
-                              <label htmlFor="name">Review</label>
-                              <textarea rows="7" className="PraxisNext-Bold" name="name"
-                                     onChange={this.handleChangeName}
-                                     value={this.state.userName} />
+                              <label htmlFor="description">Review</label>
+                              <textarea rows="7" className="PraxisNext-Bold" name="description"
+                                     onChange={this.handleReviewDescription}
+                                     value={this.state.reviewDescription} />
                               <span>* Write a review</span>
                           </li>
                           <li>
-                              <input type="submit" value="Review" />
+                              <input type="submit" value="Publish" />
                           </li>
                       </ul>
                   </form>
